718fa264d17b5b1f2695df563618072dc6404201
Felipe Saab <felipe.saab@e-cross.tech>
2025-10-19 15:14:54 -0300

    CDK code

---
+++ cdk/README.md
@@ -0,0 +1,36 @@
+## CDK - Infraestrutura como Código (IaC)
+
+Este projeto CDK provisiona os recursos dos hands-on 3 e 4:
+
+- Bucket S3 com leitura pública (via bucket policy)
+- Lambda `CreateShipmentApiHandler` + HTTP API POST /shipments
+- SQS `ShipmentEventsSQS`
+- Lambda `IdentifyNewTrackingsHandler` (produtor) com permissão de `sqs:SendMessage`
+- Lambda `ProcessTrackingHandler` (consumidor) com permissão S3 RW e trigger SQS
+
+### Comandos
+
+1. Instalar dependências
+
+```bash
+cd cdk && npm i
+```
+
+2. Synth (gerar CloudFormation)
+
+```bash
+npm run build && npx cdk synth
+```
+
+3. Deploy
+
+```bash
+npx cdk bootstrap
+npx cdk deploy
+```
+
+4. Destroy (opcional)
+
+```bash
+npx cdk destroy
+```
---
+++ cdk/lib/infoeste-serverless-stack.ts
@@ -0,0 +1,135 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigwv2 from "aws-cdk-lib/aws-apigatewayv2";
+import * as integrations from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as lambdaEventSources from "aws-cdk-lib/aws-lambda-event-sources";
+
+export class InfoesteServerlessStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // S3 bucket (public read for objects) - Hands-on 3
+    const shipmentsBucket = new s3.Bucket(this, "ShipmentsBucket", {
+      bucketName: `infoeste-2025-aws-serverless-${this.account}`,
+      blockPublicAccess: new s3.BlockPublicAccess({ blockPublicPolicy: false }),
+      publicReadAccess: true,
+      enforceSSL: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // Allow public read via bucket policy
+    shipmentsBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        sid: "AllowPublicRead",
+        actions: ["s3:GetObject"],
+        resources: [shipmentsBucket.arnForObjects("*")],
+        principals: [new iam.AnyPrincipal()],
+        effect: iam.Effect.ALLOW,
+      })
+    );
+
+    // Lambda: CreateShipmentApiHandler (Hands-on 3)
+    const createShipmentFn = new lambda.Function(
+      this,
+      "CreateShipmentApiHandlerFn",
+      {
+        functionName: "CreateShipmentApiHandler",
+        runtime: lambda.Runtime.NODEJS_22_X,
+        handler: "dist/index.handler",
+        code: lambda.Code.fromAsset(
+          "../hands-on-3/CreateShipmentApiHandler/bundle.zip"
+        ),
+        memorySize: 128,
+        timeout: cdk.Duration.seconds(10),
+        environment: {
+          SHIPMENTS_BUCKET_NAME: shipmentsBucket.bucketName,
+        },
+      }
+    );
+
+    // Grant permission to put objects in the bucket
+    shipmentsBucket.grantPut(createShipmentFn);
+
+    // HTTP API with POST /shipments integration
+    const httpApi = new apigwv2.HttpApi(this, "ShipmentsHttpApi", {
+      apiName: "Shipments API",
+    });
+    httpApi.addRoutes({
+      path: "/shipments",
+      methods: [apigwv2.HttpMethod.POST],
+      integration: new integrations.HttpLambdaIntegration(
+        "CreateShipmentIntegration",
+        createShipmentFn
+      ),
+    });
+
+    // SQS queue (Hands-on 4)
+    const shipmentEventsQueue = new sqs.Queue(this, "ShipmentEventsSQS", {
+      queueName: "ShipmentEventsSQS",
+      visibilityTimeout: cdk.Duration.seconds(60),
+      retentionPeriod: cdk.Duration.days(4),
+    });
+
+    // Lambda: IdentifyNewTrackingsHandler (producer)
+    const identifyFn = new lambda.Function(
+      this,
+      "IdentifyNewTrackingsHandlerFn",
+      {
+        functionName: "IdentifyNewTrackingsHandler",
+        runtime: lambda.Runtime.NODEJS_22_X,
+        handler: "dist/index.handler",
+        code: lambda.Code.fromAsset(
+          "../hands-on-4/IdentifyNewTrackingsHandler/bundle.zip"
+        ),
+        memorySize: 128,
+        timeout: cdk.Duration.seconds(10),
+        environment: {
+          TRACKINGS_QUEUE_URL: shipmentEventsQueue.queueUrl,
+        },
+      }
+    );
+
+    // Grant permission to send messages to the queue
+    shipmentEventsQueue.grantSendMessages(identifyFn);
+
+    // Lambda: ProcessTrackingHandler (consumer)
+    const processFn = new lambda.Function(this, "ProcessTrackingHandlerFn", {
+      functionName: "ProcessTrackingHandler",
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: "dist/index.handler",
+      code: lambda.Code.fromAsset(
+        "../hands-on-4/ProcessTrackingHandler/bundle.zip"
+      ),
+      memorySize: 256,
+      timeout: cdk.Duration.seconds(30),
+      environment: {
+        SHIPMENTS_BUCKET_NAME: shipmentsBucket.bucketName,
+      },
+    });
+
+    // Grant permission to read and write objects in the bucket
+    shipmentsBucket.grantReadWrite(processFn);
+
+    // Add event source (SQS -> ProcessTrackingHandler)
+    processFn.addEventSource(
+      new lambdaEventSources.SqsEventSource(shipmentEventsQueue, {
+        batchSize: 10,
+        enabled: true,
+      })
+    );
+
+    // Outputs
+    new cdk.CfnOutput(this, "BucketName", {
+      value: shipmentsBucket.bucketName,
+    });
+    new cdk.CfnOutput(this, "HttpApiUrl", { value: httpApi.apiEndpoint });
+    new cdk.CfnOutput(this, "ShipmentEventsQueueUrl", {
+      value: shipmentEventsQueue.queueUrl,
+    });
+  }
+}
